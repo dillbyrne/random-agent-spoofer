643f4c5d37888d0ca7e6e846d0fbc1a7306608db
dillbyrne <dbyrne@hush.com>
2014-03-05 19:37:52 +0000

    Added support for spoofing custom IPs for XFF and Via headers. #34

--- data/js/optionspage.js
+++ data/js/optionspage.js
@@ -1,66 +1,177 @@
 document.body.addEventListener("change", function(e) {
  
-  //get selected checkbox
-  if (e.target.type == "checkbox"){
-    self.port.emit(e.target.id+"cb",document.getElementById(e.target.id).checked);
-  }  
-  else{
-    
-    //get timer and selected ua option
-     var timerdd = document.getElementById("timerdd");
-     var uaList = document.getElementsByName("ua");
-    
-     var time = timerdd[timerdd.selectedIndex].value;
+	//get selected checkbox
+	if (e.target.type == "checkbox"){
+		self.port.emit(e.target.id+"cb",document.getElementById(e.target.id).checked);
+	}  
+	else if(e.target.className == "ipdropdown"){
+		
+		//get the spoofed ip choice
+		var ipchoice = e.target[e.target.selectedIndex].value;
+		var ipentry = document.getElementById("custom"+(e.target.id).substr(0,3));
+		
+		//if custom show user the entry box
+		if (ipchoice == "custom"){
+			ipentry.className="";
+		}else{
+			ipentry.className="hidden";
+		}
 
-     //get selected UA
-     var ua_choice;
-      for(var i = 0; i < uaList.length; i++){
-	if(uaList[i].checked){
-	  ua_choice = uaList[i].value;
+		self.port.emit("ipdd",e.target.id,ipchoice);
 	}
-      }
-     self.port.emit("uachange",ua_choice,time);
+	else if (e.target.id == "timerdd" || e.target.name == "ua") {
+    
+		//get timer and selected ua option
+		var timerdd = document.getElementById("timerdd");
+		var uaList = document.getElementsByName("ua");
+    
+		var time = timerdd[timerdd.selectedIndex].value;
+
+		//get selected UA
+		var ua_choice;
+		for(var i = 0; i < uaList.length; i++){
+			if(uaList[i].checked){
+				ua_choice = uaList[i].value;
+			}
+		}
+     
+		self.port.emit("uachange",ua_choice,time);
   
-  }
+	}	
 
 
 },false);
 
 
+document.body.addEventListener("keyup", function(e) {
+ 
+	//set the input validation class
+	if ((e.target.id).substr(3,2) == "ip"){
+		var input =  document.getElementById(e.target.id);
+		//console.log(input.value);
+		var result = validateIP(input.value);
+
+		if (result == false){
+			input.className = "invalidInput";
+		}else{
+			input.className = "validInput";
+			self.port.emit("validcustomip",e.target.id,input.value);
+		}
+			
+	}  
+
+
+},false);
+
+
+document.body.addEventListener("focus", function(e) {
+ 
+	//set the input validation class
+	if ((e.target.id).substr(3,2) == "ip"){
+		var input =  document.getElementById(e.target.id);
+		var result = validateIP(input.value);
+
+		if (result == false)
+			input.className = "invalidInput";
+		else
+			input.className = "validInput";
+			
+	}  
+
+
+},true);
+
+
+document.body.addEventListener("blur", function(e) {
+ 
+	//remove the class for input validation
+	if ((e.target.id).substr(3,2) == "ip"){
+		document.getElementById(e.target.id).className = "";
+	}  
+
+},true);
+
+
 function toggleList(innerListElementId) {
   
-  var innerlistElement = document.getElementById(innerListElementId);
-  
-  //get the span of the parent li 
-  //it's index is the same as it's child list, so we can get it from that
-  var text = document.getElementById("li_text"+ innerListElementId.replace( /^\D+/g, '')); 
+	var innerlistElement = document.getElementById(innerListElementId);
+	  
+	//get the span of the parent li 
+	//it's index is the same as it's child list, so we can get it from that
+	var text = document.getElementById("li_text"+ innerListElementId.replace( /^\D+/g, '')); 
 
-  //toggle the child list and parents assiociated indicator
-  if (innerlistElement.style.display == "none"){
-      innerlistElement.style.display = "block";
-      text.innerHTML = " -";
-  }else{
-      innerlistElement.style.display = "none";
-      text.innerHTML = " +";
-  }
+	//toggle the child list and parents assiociated indicator
+	if (innerlistElement.style.display == "none"){
+		innerlistElement.style.display = "block";
+		text.innerHTML = " -";
+	}else{
+		innerlistElement.style.display = "none";
+		text.innerHTML = " +";
+	}
 }
 
 function changeTab(selected_tab,tabs){
 
-  for (var i =0; i< tabs.length;i++){
-    if (tabs[i].children[0].id == selected_tab.id){
+	for (var i =0; i< tabs.length;i++){
+		if (tabs[i].children[0].id == selected_tab.id){
       
-      var selected = document.getElementById(selected_tab.id+"_content");
-      selected.className = "tabContent";
-      selected_tab.className = "selected";
+			var selected = document.getElementById(selected_tab.id+"_content");
+			selected.className = "tabContent";
+			selected_tab.className = "selected";
 
-    }else{
-      var non_selected = document.getElementById(tabs[i].children[0].id+"_content");
-      non_selected.className = "hidden";
-      tabs[i].children[0].className = "nonselected"; 
-    }
+		}else{
+			var non_selected = document.getElementById(tabs[i].children[0].id+"_content");
+			non_selected.className = "hidden";
+			tabs[i].children[0].className = "nonselected"; 
+		}
          
-  }
+	}
 
+
+}
+
+function validateIP(ipaddress){
+	
+	try	{
+		var ip_segments;
+		var valid_segments = 0;
+
+		if(ipaddress === null || ipaddress == ""){
+			throw("blankIPException");
+		}
+
+		ip_segments = ipaddress.split(".");
+		
+		//check for 4 segments split on "."
+		if(ip_segments.length != 4){
+			throw("IpSegmentLengthException");
+		}else{
+			
+			for (var i=0; i<4; i++){
+				//check if ip segment is a number and not a hex number or a
+				//space or an exponent
+				if( (!isNaN(ip_segments[i])) && ip_segments[i].indexOf('x') == -1 && ip_segments[i].length > 0
+						&& ip_segments[i].indexOf(' ') == -1 && ip_segments[i].indexOf('e') == -1 ){
+					
+					//check the range of the segment is valid
+					if(ip_segments[i] >=0 && ip_segments[i] <= 255 ){
+						valid_segments++;
+					}else{
+						throw("NumberOutOfRangeIpSegmentException");
+					}
+				}
+				else{
+					throw ("NonNumericIpSegmentException");
+				}		
+			}
+		}
+		
+		if (valid_segments == 4){
+			return true;
+		}
+	}
+	catch(e){
+		return false;
+	}
 
 }
--- data/js/set-options.js
+++ data/js/set-options.js
@@ -1,126 +1,136 @@
 self.port.on("restore-options",function(options){
 
-  //set UA Tabs elements
-  var timerdd = document.getElementById('timerdd');
+	//Set UA Profile Tabe Elements
+	setSelectedIndexByValue('timerdd',options[0]);
+	document.getElementById(options[1]).checked = true;
+	document.getElementById('notify').checked = options[2];
 
-
-  for (var i = 0; i < timerdd.options.length; i++) {
-    
-    if (timerdd.options[i].value === options[0]) {
-      timerdd.selectedIndex = i;
-      break;
-    }
-  }
-
-  document.getElementById(options[1]).checked = true;
-  document.getElementById('notify').checked = options[2];
-
-  //set Extras Tab Elements
-  document.getElementById('fonts').checked = options[3];
-  document.getElementById('dom').checked = options[4];
-  document.getElementById('history').checked = options[5];
-  document.getElementById('cache').checked = options[6];
-  document.getElementById('geo').checked = options[7];
-  document.getElementById('dns').checked = options[11];
-  document.getElementById('link').checked = options[12];
+	//Set Extras Tab Elements
+	document.getElementById('fonts').checked = options[3];
+	document.getElementById('dom').checked = options[4];
+	document.getElementById('history').checked = options[5];
+	document.getElementById('cache').checked = options[6];
+	document.getElementById('geo').checked = options[7];
+	document.getElementById('dns').checked = options[11];
+	document.getElementById('link').checked = options[12];
+	
+	//Set Header Tab Elements
+	document.getElementById('xff').checked = options[8];
+	document.getElementById('via').checked = options[9];
+	document.getElementById('ifnone').checked = options[10];
+	document.getElementById('acceptd').checked = options[13];
+	document.getElementById('accepte').checked = options[14];
+	document.getElementById('acceptl').checked = options[15];
+	document.getElementById('spoof').checked = options[16];
+	setSelectedIndexByValue('xffdd',options[17]);
+	document.getElementById('xffip').value = options[18];
+	setSelectedIndexByValue('viadd',options[19]);
+	document.getElementById('viaip').value = options[20];
   
-  //header spoofing
-  document.getElementById('xff').checked = options[8];
-  document.getElementById('via').checked = options[9];
-  document.getElementById('ifnone').checked = options[10];
-  document.getElementById('acceptd').checked = options[13];
-  document.getElementById('accepte').checked = options[14];
-  document.getElementById('acceptl').checked = options[15];
-  document.getElementById('spoof').checked = options[16];
-  
-  
+	//set custom ipcheckboxes to show if custom is selected
+	if(options[17] == "custom")
+		document.getElementById('customxff').className="";
+
+	if(options[19] == "custom")
+		document.getElementById('customvia').className="";
 });
 
 self.port.once('tab_listener',function(){
-  //get all the tabs
-  var tabs = document.getElementById("tabs").children;
+	//get all the tabs
+	var tabs = document.getElementById("tabs").children;
 
-  //add on click listener to each
-  for (var i =0; i< tabs.length;i++){
-    tabs[i].onclick = function(x,y) { return function() { changeTab(x,y); }; }(tabs[i].children[0],tabs);
-  } 
+	//add on click listener to each
+	for (var i =0; i< tabs.length;i++){
+		tabs[i].onclick = function(x,y) { return function() { changeTab(x,y); }; }(tabs[i].children[0],tabs);
+	}	 
 });
 
 
 
 self.port.once('ua_list', function(data) {
    
-    //create the list of browser profiles
+	//create the list of browser profiles
     
     var ualist_div  = document.getElementById('ualist');  
     
     //outer list
     var listElement = document.createElement("ul");
 
-    ualist_div.appendChild(listElement);
+	ualist_div.appendChild(listElement);
 
     for (var i = 0; i < data.uadata.length; i++){
 
-      var listItem = document.createElement("li");
+		var listItem = document.createElement("li");
      
-      var b = document.createElement("b");
-      var textSpan = document.createElement("span");
-      var indicatorSpan = document.createElement("span");
+		var b = document.createElement("b");
+		var textSpan = document.createElement("span");
+		var indicatorSpan = document.createElement("span");
 
-      textSpan.appendChild(document.createTextNode(data.uadata[i].description));
-      textSpan.setAttribute("class","parentli");
-      indicatorSpan.appendChild(document.createTextNode(" +"));
-      indicatorSpan.setAttribute("id","li_text"+i);
+		textSpan.appendChild(document.createTextNode(data.uadata[i].description));
+		textSpan.setAttribute("class","parentli");
+		indicatorSpan.appendChild(document.createTextNode(" +"));
+		indicatorSpan.setAttribute("id","li_text"+i);
       
-      textSpan.appendChild(indicatorSpan);
-      b.appendChild(textSpan);
-      listItem.appendChild(b);
+		textSpan.appendChild(indicatorSpan);
+		b.appendChild(textSpan);
+		listItem.appendChild(b);
 
       
-      //inner List  
-      var innerListElement = document.createElement("ul");
-      innerListElement.setAttribute("class","innerlist");
-      innerListElement.setAttribute("id","innerlist"+i);
-
-      //set the inline style to none 
-      //this prevents the need for two clicks to open the list
-      innerListElement.style.display = "none";
-
-      //show or hide inner list element when the list item it is appended to is clicked
-      listItem.onclick = function(x) { return function() { toggleList(x); }; }(innerListElement.id);
-      
-      for (var j=0; j< data.uadata[i].useragents.length; j++){
+		//inner List  
+		var innerListElement = document.createElement("ul");
+		innerListElement.setAttribute("class","innerlist");
+		innerListElement.setAttribute("id","innerlist"+i);
 	
-	var innerListItem = document.createElement("li");
-	
-	//pass the item index for the parent and for child as the value and id
-	var radio = document.createElement("input");
-	radio.setAttribute("name","ua");	
-	radio.setAttribute("type","radio");	
-	radio.setAttribute("id",i+","+j);	
-	radio.setAttribute("value",i+","+j);
-	
-	var label = document.createElement("label");
-	label.setAttribute("for",i+","+j);
-	label.appendChild(document.createTextNode(data.uadata[i].useragents[j].description));
+		//set the inline style to none 
+		//this prevents the need for two clicks to open the list
+		innerListElement.style.display = "none";
 
-	innerListItem.appendChild(radio);
-	innerListItem.appendChild(label);
-
-	//prevent clicks on child element triggering the showing/hiding of the
-	//child list	
-	innerListElement.onclick= function stopProp (event){
-	  event.stopPropagation();
-	}
-
-	innerListElement.appendChild(innerListItem);
-
-      }
+		//show or hide inner list element when the list item it is appended to is clicked
+		listItem.onclick = function(x) { return function() { toggleList(x); }; }(innerListElement.id);
       
-      listItem.appendChild(innerListElement);
-      listElement.appendChild(listItem);
+		for (var j=0; j< data.uadata[i].useragents.length; j++){
+	
+			var innerListItem = document.createElement("li");
+	
+			//pass the item index for the parent and for child as the value and id
+			var radio = document.createElement("input");
+			radio.setAttribute("name","ua");	
+			radio.setAttribute("type","radio");	
+			radio.setAttribute("id",i+","+j);	
+			radio.setAttribute("value",i+","+j);
+	
+			var label = document.createElement("label");
+			label.setAttribute("for",i+","+j);
+			label.appendChild(document.createTextNode(data.uadata[i].useragents[j].description));
+
+			innerListItem.appendChild(radio);
+			innerListItem.appendChild(label);
+
+			//prevent clicks on child element triggering the showing/hiding of the
+			//child list	
+			innerListElement.onclick= function stopProp (event){
+				event.stopPropagation();
+			}	
+
+			innerListElement.appendChild(innerListItem);
+		}
+      
+		listItem.appendChild(innerListElement);
+		listElement.appendChild(listItem);
 
     }
 
 });
 
+function setSelectedIndexByValue(dropdown,indexvalue){
+
+	var dd = document.getElementById(dropdown);
+
+	for (var i = 0; i < dd.options.length; i++) {
+	
+		if (dd.options[i].value === indexvalue) {
+			dd.selectedIndex = i;
+			break;
+		}
+	}
+};
