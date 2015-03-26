fc27569fc568e4f67edd871572fb35b2c41e8c5f
dillbyrne <dillbyrne@users.noreply.github.com>
2015-03-26 20:09:11 +0000

    Updated Firefox Profiles. Retabbed some files and removed trailing spaces

--- data/js/inject.js
+++ data/js/inject.js
@@ -1,6 +1,6 @@
 self.port.on("inject", function( intParams, strParams, boolParams) {
 
-  	//build script to inject into the page with users chosen values
+	//build script to inject into the page with users chosen values
 	var script = document.createElement( "script" );
 	script.type = "text/javascript";
 
@@ -9,63 +9,63 @@ self.port.on("inject", function( intParams, strParams, boolParams) {
 
 		//TODO set locale string using spoofed language. Needs issue #67 to be implemented first
 
-	    // Date string functions
+		// Date string functions
 		var content =  "Object.defineProperty( Date.prototype, 'toString', {value: function(){return \""+strParams[9]+"\";}});";
-		content +=  "Object.defineProperty( Date.prototype, 'toLocaleString', {value: function(){return \""+strParams[10]+"\";}});";
-		content +=  "Object.defineProperty( Date.prototype, 'toLocaleDateString', {value: function(){return \""+strParams[11]+"\";}});";
-		content +=  "Object.defineProperty( Date.prototype, 'toTimeString', {value: function(){return \""+strParams[12]+"\";}});";
-		content +=  "Object.defineProperty( Date.prototype, 'toLocaleTimeString', {value: function(){return \""+strParams[13]+"\";}});";
+		content +=	"Object.defineProperty( Date.prototype, 'toLocaleString', {value: function(){return \""+strParams[10]+"\";}});";
+		content +=	"Object.defineProperty( Date.prototype, 'toLocaleDateString', {value: function(){return \""+strParams[11]+"\";}});";
+		content +=	"Object.defineProperty( Date.prototype, 'toTimeString', {value: function(){return \""+strParams[12]+"\";}});";
+		content +=	"Object.defineProperty( Date.prototype, 'toLocaleTimeString', {value: function(){return \""+strParams[13]+"\";}});";
 
 		// time zone offset
-		content +=  "Object.defineProperty( Date.prototype, 'getTimezoneOffset', {value: function(){return "+intParams[0]+";}});";
+		content +=	"Object.defineProperty( Date.prototype, 'getTimezoneOffset', {value: function(){return "+intParams[0]+";}});";
 
 		return content;
 	}
 
 	function windowHandler(){
 
- 		var content =  "Object.defineProperty( screen, 'width', {value: "+intParams[1]+"});";
-		content +=  "Object.defineProperty( screen, 'height', {value: "+intParams[2]+"});";
-		content +=  "Object.defineProperty( screen, 'availWidth', {value: "+intParams[3]+"});";
-		content +=  "Object.defineProperty( screen, 'availHeight', {value: "+intParams[4]+"});";
-		content +=  "Object.defineProperty( window, 'innerWidth', {value: "+intParams[5]+"});";
-		content +=  "Object.defineProperty( window, 'innerHeight', {value: "+intParams[6]+"});";
-		content +=  "Object.defineProperty( window, 'outerWidth', {value: "+intParams[7]+"});";
-		content +=  "Object.defineProperty( window, 'outerHeight', {value: "+intParams[8]+"});";
-		content +=  "Object.defineProperty( screen, 'colorDepth', {value: "+intParams[9]+"});";
-		content +=  "Object.defineProperty( screen, 'pixelDepth', {value: "+intParams[10]+"});";
+		var content =  "Object.defineProperty( screen, 'width', {value: "+intParams[1]+"});";
+		content +=	"Object.defineProperty( screen, 'height', {value: "+intParams[2]+"});";
+		content +=	"Object.defineProperty( screen, 'availWidth', {value: "+intParams[3]+"});";
+		content +=	"Object.defineProperty( screen, 'availHeight', {value: "+intParams[4]+"});";
+		content +=	"Object.defineProperty( window, 'innerWidth', {value: "+intParams[5]+"});";
+		content +=	"Object.defineProperty( window, 'innerHeight', {value: "+intParams[6]+"});";
+		content +=	"Object.defineProperty( window, 'outerWidth', {value: "+intParams[7]+"});";
+		content +=	"Object.defineProperty( window, 'outerHeight', {value: "+intParams[8]+"});";
+		content +=	"Object.defineProperty( screen, 'colorDepth', {value: "+intParams[9]+"});";
+		content +=	"Object.defineProperty( screen, 'pixelDepth', {value: "+intParams[10]+"});";
 
-	    return content;
+		return content;
 
 	}
 
 	function canvasHandler(){
 
 		var content =  "Object.defineProperty( CanvasRenderingContext2D.prototype, 'fillRect', {value: function(){return undefined;}});";
-		content +=  "Object.defineProperty( CanvasRenderingContext2D.prototype, 'textBassline', {value: undefined});";
-		content +=  "Object.defineProperty( CanvasRenderingContext2D.prototype, 'font', {value: undefined});";
-		content +=  "Object.defineProperty( CanvasRenderingContext2D.prototype, 'fillStyle', {value: undefined});";
-		content +=  "Object.defineProperty( CanvasRenderingContext2D.prototype, 'strokeStyle', {value: undefined});";
-		content +=  "Object.defineProperty( CanvasRenderingContext2D.prototype, 'fillText', {value: undefined});";
-		content +=  "Object.defineProperty( HTMLCanvasElement.prototype, 'getContext', {value:function(){ return undefined;}});";
-		
+		content +=	"Object.defineProperty( CanvasRenderingContext2D.prototype, 'textBassline', {value: undefined});";
+		content +=	"Object.defineProperty( CanvasRenderingContext2D.prototype, 'font', {value: undefined});";
+		content +=	"Object.defineProperty( CanvasRenderingContext2D.prototype, 'fillStyle', {value: undefined});";
+		content +=	"Object.defineProperty( CanvasRenderingContext2D.prototype, 'strokeStyle', {value: undefined});";
+		content +=	"Object.defineProperty( CanvasRenderingContext2D.prototype, 'fillText', {value: undefined});";
+		content +=	"Object.defineProperty( HTMLCanvasElement.prototype, 'getContext', {value:function(){ return undefined;}});";
+
 		return content;
 	}
 
 	function whiteListHandler(){
 
 		var content =  "Object.defineProperty( navigator, 'userAgent', {value: \""+strParams[1]+"\"});";
-		content +=  "Object.defineProperty( navigator, 'appCodeName', {value: \""+strParams[2]+"\"});";
-		content +=  "Object.defineProperty( navigator, 'appName', {value: \""+strParams[3]+"\"});";
-		content +=  "Object.defineProperty( navigator, 'appVersion', {value: \""+strParams[4]+"\"});";
-		content +=  "Object.defineProperty( navigator, 'vendor', {value: \""+strParams[5]+"\"});";
-		content +=  "Object.defineProperty( navigator, 'vendorSub', {value: \""+strParams[6]+"\"});";
-		content +=  "Object.defineProperty( navigator, 'platform', {value: \""+strParams[7]+"\"});";
-		content +=  "Object.defineProperty( navigator, 'oscpu', {value: \""+strParams[8]+"\"});";
+		content +=	"Object.defineProperty( navigator, 'appCodeName', {value: \""+strParams[2]+"\"});";
+		content +=	"Object.defineProperty( navigator, 'appName', {value: \""+strParams[3]+"\"});";
+		content +=	"Object.defineProperty( navigator, 'appVersion', {value: \""+strParams[4]+"\"});";
+		content +=	"Object.defineProperty( navigator, 'vendor', {value: \""+strParams[5]+"\"});";
+		content +=	"Object.defineProperty( navigator, 'vendorSub', {value: \""+strParams[6]+"\"});";
+		content +=	"Object.defineProperty( navigator, 'platform', {value: \""+strParams[7]+"\"});";
+		content +=	"Object.defineProperty( navigator, 'oscpu', {value: \""+strParams[8]+"\"});";
 
 		return content;
 	}
-	
+
 	/*
 	//TODO
 	function pluginHandler(){
@@ -82,21 +82,21 @@ self.port.on("inject", function( intParams, strParams, boolParams) {
 	}
 	*/
 
-	var content = "(function (){try{"  
-	
+	var content = "(function (){try{"
+
 	// Use whitelisted profile (if selected)
 	if (boolParams[0] == true){
 		content += whiteListHandler();
 	}else{
 		// spoof as normal
 		// restore vendor functionality
-		content +=  "Object.defineProperty( navigator, 'vendor', {value: \""+strParams[0]+"\"});";
-		//TODO set navigator.language equal to the accept language header 
+		content +=	"Object.defineProperty( navigator, 'vendor', {value: \""+strParams[0]+"\"});";
+		//TODO set navigator.language equal to the accept language header
 	}
 
 	//blank out the productSub property
 	content += "Object.defineProperty( navigator, 'productSub', {value: \"\"});";
-	
+
 	//Disable canvas support (if selected)
 	if (boolParams[1] == true) {
 		content += canvasHandler();
@@ -104,13 +104,13 @@ self.port.on("inject", function( intParams, strParams, boolParams) {
 
 	//Reset window.name on each request (if selected)
 	if (boolParams[2] == true){
-		content +=  "Object.defineProperty( window, 'name', {value: \"\", writable: true});";
+		content +=	"Object.defineProperty( window, 'name', {value: \"\", writable: true});";
 	}
 
 	// screen & window prefrences (if selected)
 	if (boolParams[3] == true){
 
-		if(intParams[1] != null){ 
+		if(intParams[1] != null){
 			content += windowHandler();
 		}
 	}
@@ -122,10 +122,10 @@ self.port.on("inject", function( intParams, strParams, boolParams) {
 
 	//remove script after modifications to prevent sites from reading it
 	content += "var ras_script = document.getElementsByTagName('script')[0]; ras_script.parentNode.removeChild(ras_script);";
-	content +=  "} catch (e) {} }) ();"
+	content +=	"} catch (e) {} }) ();"
 
 	script.textContent = content;
-	
+
 	//Append script as first child of the head element
 	var head = window.document.head;
 	head.insertBefore(script, head.firstChild);
