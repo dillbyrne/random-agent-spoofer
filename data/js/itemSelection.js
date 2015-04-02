1023912f0d560f4927255489689c9a2857c9111f
dillbyrne <dillbyrne@users.noreply.github.com>
2015-04-02 11:11:59 +0100

    #163 Added option to use temporary email addresses. #154 Changed RAS  icons to match the new UI.

--- data/js/itemSelection.js
+++ data/js/itemSelection.js
@@ -1,3 +1,43 @@
 self.on("click", function (node, data) {
+
+	//generate temporary email addresses
+	if(data.slice(0,5) === "mail_"){
+		data = setupTempEmail(data,node);
+	}
+	else{
+		data = [data,null];
+	}
+
 	self.postMessage(data);
 });
+
+function getRandomStr(){
+	return Math.random().toString(36).replace(/[^a-z]+/g, '').substr(0, 9);
+}
+
+function setupTempEmail(data,node){
+	var mailbox_id = getRandomStr();
+	var url = "";	
+
+	if(data === "mail_anonbox"){
+		node.value=mailbox_id+"@anonbox.net";
+		url = "https://anonbox.net/abx.html?url="+mailbox_id;
+
+	}else if(data === "mail_kozmail"){
+		//pick a server randomly
+		var server = ((Math.round(Math.random()*10) + 1) % 2 == 0 ) ? "@ee1.pl":"@ee2.pl";
+		node.value = mailbox_id+server;
+		url = "http://www.koszmail.pl/koszmail/mailBox.php?box="+mailbox_id+"&mailBoxAt="+server;
+
+	}else if(data === "mail_dispostable"){
+		node.value = mailbox_id+"@dispostable.com";
+		url = "http://www.dispostable.com/inbox/"+mailbox_id+"/";
+
+	}else if(data === "mail_mailcatch"){
+		node.value = mailbox_id+"@mailcatch.com";
+		url = "http://www.mailcatch.com/en/temporary-inbox?box="+mailbox_id;
+	}
+	
+	return [data,url];
+	
+}
