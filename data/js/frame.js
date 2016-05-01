f5cd12e2ffcb2e51b48b154ab129257b9b7e7e4a
dillbyrne <dillbyrne@users.noreply.github.com>
2016-05-01 14:03:55 +0100

    #365 Disabled pagemod based script injection, initial frame script based injection is working, canvas blocking has been restored

---
+++ data/js/frame.js
@@ -0,0 +1,38 @@
+/*Based up on the work done by kkapsner in canvas blocker */
+
+(function(){
+
+	const {utils: Cu} = Components;
+	const {require} = Cu.import("resource://gre/modules/commonjs/toolkit/require.js", {});
+	const {intercept} = require("../../lib/intercept.js");	
+	var frameScriptEnabled = true, 
+	context = this;
+
+	function interceptWrapper(ev){
+		if (frameScriptEnabled){
+			// window is only equal to content for the top window. For susequent
+			// calls (e.g. iframe windows) the new generated window has to be 
+			// used.
+			
+			var window = ev.target.defaultView;
+			intercept({subject: window},sendSyncMessage("RAS-frameOptions")[0]);
+			
+		}
+	}
+
+
+	addEventListener("DOMWindowCreated", interceptWrapper);
+	addEventListener("unload", function(ev){
+
+		if (ev.target === context){
+			removeEventListener("DOMWindowCreated", interceptWrapper);
+		}
+	});
+	
+	addMessageListener("RAS-unload", function unload(){
+		frameScriptEnabled = false;
+		removeEventListener("DOMWindowCreated", interceptWrapper);
+		removeMessageListener("RAS-unload", unload);
+	});
+
+}());
